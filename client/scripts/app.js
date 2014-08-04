36311470c04abc5676db54be935b21438f154f1f
Hack Reactor Students <klp2012@gmail.com>
2014-08-04 14:46:07 -0700

    passed sending, fetching specs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,3 +1,31 @@
 // YOUR CODE HERE:
-var app = {};
+var app = {
+  server : "https://api.parse.com/1/classes/chatterbox"
+};
+
 app.init = function(){};
+
+
+app.send = function(message){
+  $.ajax({
+    type: "POST",
+    url: this.server,
+    data: JSON.stringify(message),
+    success: console.log("YES"),
+    dataType: JSON
+  });
+};
+
+app.fetch = function(data){
+  $.ajax({
+    type: "GET",
+    url: this.server,
+    data: data,
+    success: console.log('got it', data),
+    dataType: JSON
+  });
+};
+
+app.clearMessages = function(){
+  $('#chats').children().empty();
+}
